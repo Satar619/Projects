765d354d3fb785ab76976bbc320b32d9ae645e69
satar619 <abdulsatarsherzai300@gmail.com>
2019-06-10 19:48:24 +0200

    new file

---
+++ CodeDoor-front-end-challenge-03.2019/js/main.js
@@ -0,0 +1,19 @@
+
+
+        /* Cookies*/
+    window.addEventListener("load", function(){
+    window.cookieconsent.initialise({
+      "palette": {
+        "popup": {
+          "background": "#3c404d",
+          "text": "#d6d6d6"
+        },
+        "button": {
+          "background": "#8bed4f"
+        }
+      },
+      "theme": "classic",
+      "content": {
+        "href": "https://codedoor.org/privacy.html"
+      }
+    })});
