57beb1993de05f6c2bd91615fe23d679c76b215f
SatarShirzai <abdulsatarsherzai@gmail.com>
2019-12-11 19:25:27 +0100

    CookieApp added

---
+++ CookieApp/assets/data/cookies.js
@@ -0,0 +1,271 @@
+export const cookies = [{
+    name: "Cookie Blue",
+    image: "https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Red",
+    image: "https://images.unsplash.com/photo-1530751127259-074b0cdc0469?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Yellow",
+    image: "https://images.unsplash.com/photo-1548365328-8c6db3220e4c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Green",
+    image: "https://images.unsplash.com/photo-1491374812364-00028bbe7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Black",
+    image: "https://images.unsplash.com/photo-1515265705-cbcc6e6ba264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Grey",
+    image: "https://images.unsplash.com/photo-1513806562344-c75007ad303f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Orange",
+    image: "https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie White",
+    image: "https://images.unsplash.com/photo-1530751127259-074b0cdc0469?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Purple",
+    image: "https://images.unsplash.com/photo-1548365328-8c6db3220e4c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Lime",
+    image: "https://images.unsplash.com/photo-1491374812364-00028bbe7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Brown",
+    image: "https://images.unsplash.com/photo-1515265705-cbcc6e6ba264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Pink",
+    image: "https://images.unsplash.com/photo-1513806562344-c75007ad303f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Gold",
+    image: "https://images.unsplash.com/photo-1532499016263-f2c3e89de9cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Silver",
+    image: "https://images.unsplash.com/photo-1530751127259-074b0cdc0469?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit.Rem, nemo.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Violet",
+    image: "https://images.unsplash.com/photo-1548365328-8c6db3220e4c?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Teal",
+    image: "https://images.unsplash.com/photo-1491374812364-00028bbe7d2f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias provident vero voluptatum consequatur fugit saepe itaque autem cupiditate, est mollitia error unde asperiores praesentium eveniet tempore ipsum culpa repudiandae. Eaque a aperiam cupiditate ratione id praesentium vitae, quam quae.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Aqua",
+    image: "https://images.unsplash.com/photo-1515265705-cbcc6e6ba264?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni corporis eos, voluptas officia hic dicta? Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur suscipit similique praesentium dolorum veniam. Enim aliquid, ad ullam tempore consequuntur eos pariatur amet porro voluptas iusto molestias nihil omnis.",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  },
+  {
+    name: "Cookie Space",
+    image: "https://images.unsplash.com/photo-1513806562344-c75007ad303f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum corrupti fuga quaerat perspiciatis, enim dolorum aperiam aut? Quas obcaecati facere, quasi nostrum iure atque ipsam, debitis ut aspernatur pariatur, dolorem natus odit minima! Voluptatem fuga esse ab ullam cumque debitis reprehenderit quidem non nihil accusamus?",
+    ingredients: [
+      "2 cups diced cooked turkey",
+      "2 celery ribs, diced",
+      "1 small onion, diced",
+      "2 hard-cooked eggs, chopped",
+      "3/4 cup mayonnaise",
+      "1/2 teaspoon salt",
+      "1/4 teaspoon pepper",
+      "6 hamburger buns, split"
+    ]
+  }
+];
---
+++ CookieApp/assets/js/index.js
@@ -0,0 +1,141 @@
+// Import cookies data from assets/data/cookies.js
+import {
+  cookies
+} from "../data/cookies.js";
+
+// ============================================
+// Display Cookies Feature
+// ============================================
+
+// Select div which to append cookies data
+let container = document.querySelector(".slider-container");
+
+// Loop over cookies data and append new divs accordingly
+for (let i = 0; i < cookies.length; i++) {
+  // Create new div
+  let div = document.createElement("div");
+  // Add class card
+  div.classList.add("card");
+
+  // Add individual cookie details in div
+  div.innerHTML = `
+  <img src=${cookies[i].image}>
+  <div class="content">
+    <h4 class="cookie-name">${cookies[i].name}</h4>
+    <p>${cookies[i].description}</p>
+  </div>
+  `;
+
+  // ============================================
+  // Pop-up Feature
+  // ============================================
+
+  div.addEventListener("click", (event) => {
+    //Select all elements
+    let modal = document.getElementById("myModal"),
+      span = document.getElementsByClassName("close")[0],
+      h = document.getElementById("modalH"),
+      p = document.getElementById("modalP");
+
+    //Set name and description for modal
+    h.innerHTML = `${cookies[i].name}`;
+    p.innerHTML = `${cookies[i].description}`;
+
+    //Makes div visible
+    modal.style.display = "block";
+
+    //Close modal by clicking X
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+
+    //Close modal by clicking outside the modal window
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+  });
+
+  // Append div to .slider-container
+  container.append(div);
+}
+
+// ============================================
+// More / Less Cookies Feature
+// ============================================
+
+let cards = document.querySelectorAll(".card");
+let showMore = document.querySelector(".show-more");
+let showLess = document.querySelector(".show-less");
+
+// Default number of cookies shown
+let showNumber = 6;
+
+// Show default number of cookies
+showCookies(cards);
+
+// Show More button event listener
+showMore.addEventListener("click", () => {
+  // Validation for maximum number of cookies to be shown
+  if (showNumber < cards.length) {
+    showNumber += 6;
+    showCookies(cards);
+  } else {
+    showNumber = cards.length;
+  }
+});
+
+showLess.addEventListener("click", () => {
+  // Validation for minimum number of cookies to be shown
+  if (showNumber > 6) {
+    showNumber -= 6;
+    hideCookies(cards);
+  } else {
+    showNumber = 6;
+  }
+});
+
+// Display "showNumber" number of cookies
+function showCookies(cards) {
+  for (let i = 0; i < showNumber; i++) {
+    cards[i].classList.add("display");
+  }
+};
+
+// Hide "showNumber" number of cookies
+function hideCookies(cards) {
+  for (let i = showNumber; i < cards.length; i++) {
+    cards[i].classList.remove("display");
+  }
+};
+
+// ============================================
+// Search Feature
+// ============================================  
+
+let input = document.getElementById("searchbar");
+let cookieName = document.querySelectorAll(".cookie-name");
+
+input.addEventListener("keyup", search);
+
+function search() {
+  // Capture searched term
+  let searchTerm = input.value.toUpperCase();
+
+  if (searchTerm.length === 0) {
+    // Search term empty validation
+    hideCookies(cards);
+    showNumber = 6;
+    showCookies(cards);
+  } else {
+    for (let i = 0; i < cards.length; i++) {
+      // FInd if the searched term exists in the list of cookie names
+      if (cookieName[i].textContent.toUpperCase().indexOf(searchTerm) > -1) {
+        cards[i].classList.add("display");
+      } else {
+        cards[i].classList.remove("display");
+      }
+    }
+  }
+}
